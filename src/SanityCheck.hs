6c46c34a3b1b66c390821058e99a7a252d6b2f51
mysjkin <iarogolo@gmail.com>
2019-10-11 20:58:44 +0200

    Usage goes to end check

--- src/SanityCheck.hs
+++ src/SanityCheck.hs
@@ -3,6 +3,9 @@ module SanityCheck where
 import Data.List (group, sort, nub)
 import AST 
 import MungoParser
+import Data.Maybe
+import Data.Graph
+
 
 -- Helper functions 
 duplicates :: [String] -> [String]
@@ -30,20 +33,20 @@ sanityCheckDuplicateNames classes enums =
     map (++ " duplicated class/enum name") (duplicates names)
     where
         classNames = sort $ map className classes
-        enumNames = sort $ map enumName enums
-        names = classNames ++ enumNames
+        enumNames  = sort $ map enumName enums
+        names      = classNames ++ enumNames
 
 sanityCheckPrograms :: [CstProgram] -> [String]
 sanityCheckPrograms programs =
     duplicateNameErrs ++ fieldTypeErrs ++ methodTypeErrs ++ classErrs
     where
-        classes = concat $ map progClasses programs
-        enums = concat $ map progEnums programs
-        supportedTypes = getAllTypes classes enums
+        classes           = concat $ map progClasses programs
+        enums             = concat $ map progEnums programs
+        supportedTypes    = getAllTypes classes enums
         duplicateNameErrs = sanityCheckDuplicateNames classes enums
-        fieldTypeErrs = concat $ map (`sanityCheckFieldTypes` supportedTypes) $ map classFields classes
-        methodTypeErrs = concat $ map (`sanityCheckMethodTypes` supportedTypes) $ map classMethods classes
-        classErrs = concat $ map sanityCheckClass classes
+        fieldTypeErrs     = concat $ map (`sanityCheckFieldTypes` supportedTypes) $ map classFields classes
+        methodTypeErrs    = concat $ map (`sanityCheckMethodTypes` supportedTypes) $ map classMethods classes
+        classErrs         = concat $ map sanityCheckClass classes
 
 -- UsedTypes -> Identifier -> SupportedTypes -> ErrorMsg
 sanityCheckTypes :: [String] -> [String] -> [String] -> String -> [String]
@@ -63,21 +66,21 @@ sanityCheckFieldTypes :: [CstField] -> [String] -> [String]
 sanityCheckFieldTypes fields types =
     sanityCheckTypes usedTypes fieldNames types msg
     where
-        usedTypes = map fieldType fields
+        usedTypes  = map fieldType fields
         fieldNames = map fieldName fields
-        msg = "used in field"
+        msg        = "used in field"
 
 sanityCheckMethodTypes :: [CstMethod] -> [String] -> [String]
 sanityCheckMethodTypes methods types = 
     (sanityCheckTypes methodTypes methodNames types methodMsg)
     ++ (sanityCheckTypes parameterTypes parameterNames types parameterMsg)
     where
-        methodTypes = map methodType methods
-        methodNames = map methodName methods
-        methodMsg = "used in return of method"
+        methodTypes    = map methodType methods
+        methodNames    = map methodName methods
+        methodMsg      = "used in return of method"
         parameterTypes = map parameterType methods
         parameterNames = map parameterName methods
-        parameterMsg = "used in parameter"
+        parameterMsg   = "used in parameter"
 
 -- Enum CHECKING
     -- Unique labels
@@ -85,17 +88,18 @@ sanityCheckMethodTypes methods types =
 sanityCheckEnums :: [CstProgram] -> [String]
 sanityCheckEnums programs = 
     map (++ " duplicated label") (duplicates labels)
-    where labels = sort $ concat $ map enumLabels $ concat $ map progEnums programs
+    where 
+        labels = sort $ concat $ map enumLabels $ concat $ map progEnums programs
 
 -- CLASS CHECKING
 -- Combines errors from sub-class checkers.
 sanityCheckClass :: CstClass -> [String]
-sanityCheckClass (CstClass name usage recusage fields methods) =
+sanityCheckClass cls@(CstClass name usage recusage fields methods) =
     usageErrs ++ fieldErrs ++ methodErrs ++ parameterErrs
     where 
         usageErrs     = sanityCheckUsageRecVariables usage recusage
                         ++ sanityCheckUsageMethodExist usage recusage methods
-                        ++ sanityCheckUsageGoesToEnd usage recusage name
+                        ++ [sanityCheckUsageGoesToEnd cls]
         fieldErrs     = sanityCheckFieldVariables fields
         methodErrs    = sanityCheckMethods methods
         parameterErrs = sanityCheckParameters methods fields
@@ -143,16 +147,59 @@ sanityCheckUsageMethodExist :: CstUsage -> [(String, CstUsage)] -> [CstMethod] -
 sanityCheckUsageMethodExist usage recUsage methods = 
     map (++ " method undefined in usage") $ filter (`notElem` methodNames) $ nub $ recUsageMethods ++ usageMethodNames
     where
-        methodNames = map methodName methods
+        methodNames      = map methodName methods
         usageMethodNames = usageMethods usage
-        recUsageMethods = concat $ map usageMethods $ map snd recUsage
+        recUsageMethods  = concat $ map usageMethods $ map snd recUsage
 
-sanityCheckUsageGoesToEnd :: CstUsage -> [(String, CstUsage)] -> String -> [String]
-sanityCheckUsageGoesToEnd usage recusage className = 
-   map (++ className) $ sanityCheckUsageGoesToEnd' usage
+-- GRAPH STUFF for checking that usage goes to end.
 
-sanityCheckUsageGoesToEnd' :: CstUsage -> [String]
-sanityCheckUsageGoesToEnd' (CstUsageBranch usage) = concat $ map sanityCheckUsageGoesToEnd' $ map snd usage
-sanityCheckUsageGoesToEnd' (CstUsageChoice usage) = concat $ map sanityCheckUsageGoesToEnd' $ map snd usage
-sanityCheckUsageGoesToEnd' CstUsageEnd = []
-sanityCheckUsageGoesToEnd' _ = ["Usage does not go to end in class "]
+getNextNodes :: CstUsage -> ([String], [CstUsage])
+getNextNodes usage =
+    case usage of
+        (CstUsageBranch branchs) -> (map fst branchs, map snd branchs)
+        (CstUsageChoice choices) -> (concat $ map fst $ map getNextNodes $ map snd choices, map snd choices)
+        (CstUsageVariable var)   -> ([var], [])
+        (CstUsageEnd)            -> (["end"], [CstUsageVariable "end"])
+
+buildRecUsagePaths :: [(String, CstUsage)] -> [(String, String, [String])]
+buildRecUsagePaths (x:xs) =
+    [(s, s, nextNodes)] 
+    ++ (buildUsagePaths $ snd x)
+    ++ (buildRecUsagePaths xs)
+    where
+        s = fst x
+        (nextNodes, nextUsages) = getNextNodes $ snd x
+buildRecUsagePaths _ = []
+
+buildUsagePaths :: CstUsage -> [(String, String, [String])]
+buildUsagePaths usage =
+    (map (\p -> (fst p, fst p, nub $ snd p)) $ zip s w) 
+    ++ (concat $ map buildUsagePaths (ends ++ nextUsages))
+    where
+        currNodes   = getNextNodes usage
+        s           = fst currNodes
+        nextNodes   = map getNextNodes $ snd currNodes
+        nextUsages  = concat $ map snd nextNodes
+        ends        = filter (\x -> x == (CstUsageEnd)) (snd currNodes)
+        w           = map fst nextNodes
+
+buildUsageGraph :: CstUsage -> [(String, CstUsage)] -> (Graph, Vertex -> (String, String, [String]), String -> Maybe Vertex) 
+buildUsageGraph usage recUsage = 
+    graphFromEdges edges
+    where
+        recEdges   = buildRecUsagePaths recUsage
+        usageEdges = buildUsagePaths usage
+        edges      = recEdges ++ usageEdges
+
+sanityCheckUsageGoesToEnd :: CstClass -> String
+sanityCheckUsageGoesToEnd (CstClass name usage recUsage _ _) =
+    if (length notEnd) > 0 
+        then "Usage does not go to end in class " ++ name
+    else []
+    where
+        (g, v2n, k2v) = buildUsageGraph usage recUsage
+        end           = case (k2v "end") of
+                            Just x -> x
+                            Nothing -> -1
+        vs            = vertices g
+        notEnd        = filter (\v -> notElem end $ reachable g v) vs
